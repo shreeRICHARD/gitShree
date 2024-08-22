f9f8d652006c89ebc196e33a31e0bc993d45896c
shreerichard <shreerichard20@gmail.com>
2024-08-22 11:40:27 +0530

    if else

---
+++ if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("enter the value of a :");
+    scanf("%d", &a);
+    printf("enter the value of b :");
+    scanf("%d", &b);
+
+    if (a < b)
+    {
+        printf("a is lesser then b ");
+    } else {
+        printf("b is lesser then a ");
+    }
+}
