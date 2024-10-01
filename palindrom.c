466e153f7cc60c5c23473b864355ef117502907e
shreerichard <shreerichard20@gmail.com>
2024-10-01 17:51:16 +0530

    palindrom.c

---
+++ palindrom.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+
+int len,count,i=0;
+char a[10];
+printf("enter palword :");
+scanf("%s",&a);
+
+len=strlen(a);
+
+for(int i=0;i<len;i++){
+    if(i==a [len-i-1]){
+        count++;
+    }
+}
+if(len==count){
+    printf("palindrome");
+}
+else{
+    printf("not a palindrome ");
+}
+
+
+
+
+
+}
