97e6d5ccfc985376524c7837c3f7b20e441e941e
shreerichard <shreerichard20@gmail.com>
2024-10-03 14:59:27 +0530

    changepal

--- palindrom.c
+++ palindrom.c
@@ -1,29 +1,29 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-void main(){                      //Error?
+void main()
 
-int len,count,i=0;
-char a [10];
-printf("enter palword :");
-scanf("%s",&a);
+{ // Error?
+    int len, count=0;
+    char a[10];
+    printf("enter palword :");
+    scanf("%c", &a);
 
-len=strlen(a);
+    len = strlen(a);
 
-for(int i=0;i<len;i++){
-    if(i==a [len-i-1]){
-        count++;
+    for (int i = 0; i < len / 2; i++)
+    {
+        if (a[i] == a[len - i - 1])
+        {
+            count++;
+        }
     }
-}
-if(len==count){
-    printf("palindrome");
-}
-else{
-    printf("not a palindrome ");
-}
-
-
-
-
-
+    if (count == len / 2)
+    {
+        printf("palindrome\n");
+    }
+    else
+    {
+        printf("not a palindrome\n ");
+    }
 }
