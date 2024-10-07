143135f12c384cd9ac9da586f79851c780f49eec
shreerichard <shreerichard20@gmail.com>
2024-10-07 15:40:08 +0530

    digitedit.c

--- digit.c
+++ digit.c
@@ -1,9 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
+int findDigits(int num) {
+    int count = 0;  
+    if (num == 0)
+        return 1;
 
-void main(){
+    if (num < 0)
+        num = -num;
 
-printf("Enter the finding digite:");
+    while (num != 0) {
+        num = num / 10; 
+        count++;        
+    }
+
+    return count;      
+}
+
+int main() {
+    int number;
 
     
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+   
+    int digits = findDigits(number);
+    printf("The number of digits in %d is: %d\n", number, digits);
+
+    return 0;
 }
